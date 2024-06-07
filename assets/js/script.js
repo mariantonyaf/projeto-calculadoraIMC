68994a253a9c101fa40e9f1b3dd87347f1e589e0
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-07 09:05:40 -0300

    pjt

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', function(event){
     const weight = document.getElementById('weight').value
     const height = document.getElementById('height').value
 
-    const imc = (weight / (height * height)).toFixed(2)
+    const bmi = (weight / (height * height)).toFixed(2)
 
     const value = document.getElementById('value')
     let description = ''
